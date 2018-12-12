7b25d127a2bfd0ba97a79ff9154217fc1067ff56
dongch007 <dongch007@gmail.com>
2018-12-12 21:32:03 +0800

    update.....

--- Assets/Script/AntiPortalCuller.cs
+++ Assets/Script/AntiPortalCuller.cs
@@ -10,9 +10,11 @@ public class AntiPortalCuller : MonoBehaviour {
     [SerializeField]
     private int maxOccluderNum = 16;
 
+    [SerializeField]
+    private float minOccluderSceenThreshold = 0.001f;
+
     // Use this for initialization
     void Start () {
-
 	}
 	
 	// Update is called once per frame
@@ -29,6 +31,9 @@ public class AntiPortalCuller : MonoBehaviour {
     private List<Occluder> occluders = new List<Occluder>();
     public void AddOccluder(Occluder occluder)
     {
+        if (occluder.ScreenArea < this.minOccluderSceenThreshold)
+            return;
+
         this.occluders.Add(occluder);
     }
 
@@ -48,20 +53,38 @@ public class AntiPortalCuller : MonoBehaviour {
         for (int i = 0; i < this.occludees.Count; i++)
             this.visableFlag.Add(true);
 
+        Vector3 viewPos = this.transform.position;
+        Vector3 viewDir = this.transform.forward;
 
+        this.occluders.Sort((x, y) => -x.ScreenArea.CompareTo(y.ScreenArea));
+        int occluderNum = Mathf.Min(this.occluders.Count, this.maxOccluderNum);
+        for(int i = 0; i < occluderNum; i++)
+        {
+            Occluder occluder = this.occluders[i];
 
+            List<Plane> cullPlanes = occluder.CalculateCullPlanes(viewPos, viewDir);
+            for (int occludeeIdx = 0; occludeeIdx < this.occludees.Count; occludeeIdx++)
+            {
+                Bounds bounds = this.occludees[occludeeIdx].GetBounds();
+                int num = 0;
+                foreach (Plane plane in cullPlanes)
+                {
+                    if (this.TestPlaneAABBOutside(plane, bounds) == false)
+                        break;
 
-        for (int i = 0; i < this.occludees.Count; i++)
-            this.visableFlag[i] = false;
+                    num++;
+                }
+
+                if(num == cullPlanes.Count)
+                    this.visableFlag[occludeeIdx] = false;
+            }
+        }
 
 
         for (int i = 0; i < this.occludees.Count; i++)
         {
             this.occludees[i].SetVisable(this.visableFlag[i]);
         }
-
-        //this.occludees.Clear();
-        //this.occluders.Clear();
     }
 
     private void OnPostRender()
@@ -77,4 +100,25 @@ public class AntiPortalCuller : MonoBehaviour {
 
         this.visableFlag.Clear();
     }
+
+    private bool TestPlaneAABBOutside(Plane plane, Bounds bounds)
+    {
+        //float distance = plane.GetDistanceToPoint(bounds.center);
+        //float radius = Vector3.Dot(bounds.extents, plane.normal);
+
+        //if ((distance + radius) < 0)
+        //    return true;
+
+        //return false; 
+
+        return true;
+    }
+
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+
+    }
+#endif
 }
--- Assets/Script/Occluder.cs
+++ Assets/Script/Occluder.cs
@@ -2,14 +2,108 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Occluder : MonoBehaviour {
+public class Occluder : MonoBehaviour
+{
 
     public Vector3 center = Vector3.zero;
     public Vector3 extends = Vector3.one;
 
+    struct Face
+    {
+        public Plane plane;
+        public int[] egdes;
+        public int[] egdesCCW;
+
+        public Face(Vector3[] corners, int idx0, int idx1, int idx2, int idx3)
+        {
+            this.plane = new Plane(corners[idx0], corners[idx1], corners[idx2]);
+
+            this.egdes = new int[4];
+            this.egdes[0] = idx0 << 8 | idx1;
+            this.egdes[1] = idx1 << 8 | idx2;
+            this.egdes[2] = idx2 << 8 | idx3;
+            this.egdes[3] = idx3 << 8 | idx0;
+
+            this.egdesCCW = new int[4];
+            this.egdesCCW[0] = idx1 << 8 | idx0;
+            this.egdesCCW[1] = idx2 << 8 | idx1;
+            this.egdesCCW[2] = idx3 << 8 | idx2;
+            this.egdesCCW[3] = idx0 << 8 | idx3;
+        }
+    }
+
+    private Vector3[] corners = new Vector3[8];
+    private Face[] faces = new Face[6];
+
+    private List<Plane> cullPlanes = new List<Plane>();
+
+
+    //idx shound be clockwise
+    public void InitFace(int faceIdx, int idx0, int idx1, int idx2, int idx3)
+    {
+        Face face = this.faces[faceIdx];
+
+        Plane plane = new Plane(corners[idx0], corners[idx1], corners[idx2]);
+        face.plane = plane;
+
+
+        face.egdes[0] = idx0 << 8 | idx1;
+        face.egdes[1] = idx1 << 8 | idx2;
+        face.egdes[2] = idx2 << 8 | idx3;
+        face.egdes[3] = idx3 << 8 | idx0;
+
+        face.egdesCCW[0] = idx1 << 8 | idx0;
+        face.egdesCCW[1] = idx2 << 8 | idx1;
+        face.egdesCCW[2] = idx3 << 8 | idx2;
+        face.egdesCCW[3] = idx0 << 8 | idx3;
+    }
+
     void Start()
     {
+        //     5-------6
+        //    /|      /|
+        //   / |     / |
+        //  4--|----7  |
+        //  |  1----|--2
+        //  |  /    | /
+        //  | /     |/
+        //  0-------3
 
+        Vector3 min = this.center - extends;
+        Vector3 max = this.center + extends;
+        this.corners[0].Set(min.x, min.y, min.z);
+        this.corners[1].Set(min.x, min.y, max.z);
+        this.corners[2].Set(max.x, min.y, max.z);
+        this.corners[3].Set(max.x, min.y, min.z);
+        this.corners[4].Set(min.x, max.y, min.z);
+        this.corners[5].Set(min.x, max.y, max.z);
+        this.corners[6].Set(max.x, max.y, max.z);
+        this.corners[7].Set(max.x, max.y, min.z);
+
+        for (int i = 0; i < this.corners.Length; i++)
+        {
+            this.corners[i] = this.transform.TransformPoint(this.corners[i]);
+        }
+
+        //+X
+        this.faces[0] = new Face(this.corners, 7, 6, 2, 3);
+        //-X
+        this.faces[1] = new Face(this.corners, 5, 4, 0, 1);
+        //+Y
+        this.faces[2] = new Face(this.corners, 4, 5, 6, 7);
+        //-Y
+        this.faces[3] = new Face(this.corners, 3, 2, 1, 0);
+        //+Z
+        this.faces[4] = new Face(this.corners, 6, 5, 1, 2);
+        //-Z
+        this.faces[5] = new Face(this.corners, 0, 4, 7, 3);
+    }
+
+
+    private float screenArea = 0;
+    public float ScreenArea
+    {
+        get { return this.screenArea; }
     }
 
     private void OnWillRenderObject()
@@ -20,8 +114,118 @@ public class Occluder : MonoBehaviour {
         {
             Debug.Log("Occluder.OnWillRenderObject " + Time.frameCount);
 
+            Vector2 min = Vector2.positiveInfinity;
+            Vector2 max = Vector2.negativeInfinity;
+            foreach (Vector3 corner in this.corners)
+            {
+                Vector3 screenPos = camera.WorldToViewportPoint(corner);
+
+                //if occluder cross with camera's near plane, ignore this occluder
+                if (screenPos.z < 0)
+                    return;
+
+                if (screenPos.x < min.x)
+                    min.x = screenPos.x;
+                else if (screenPos.x > max.x)
+                    max.x = screenPos.x;
+
+                if (screenPos.y < min.y)
+                    min.y = screenPos.y;
+                else if (screenPos.y > max.y)
+                    max.y = screenPos.y;
+            }
+
+            if (min.x < -1.0f)
+                min.x = -1.0f;
+            if (min.y < -1.0f)
+                min.y = -1.0f;
+
+            if (max.x > 1.0f)
+                max.x = 1.0f;
+            if (max.y > 1.0f)
+                max.y = 1.0f;
+
+            this.screenArea = (max.x - min.x) * (max.y - min.y);
+
             culler.AddOccluder(this);
         }
+    }
+
+    HashSet<int> edges = new HashSet<int>();
+    public List<Plane> CalculateCullPlanes(Vector3 viewPos, Vector3 viewDir)
+    {
+        this.cullPlanes.Clear();
+
+        foreach (Face face in this.faces)
+        {
+            if(Vector3.Dot(face.plane.normal, viewDir) < 0)
+            {
+                this.cullPlanes.Add(face.plane);
+
+                for(int i = 0; i <4; i++)
+                {
+                    int edge = face.egdes[i];
+                    int edgeCCW = face.egdesCCW[i];
+                    if (this.edges.Contains(edgeCCW))
+                        this.edges.Remove(edgeCCW);
+                    else
+                        this.edges.Add(edge);
+                }
+            }
+        }
+
+        foreach (int edge in this.edges)
+        {
+            Vector3 v0 = this.corners[edge >> 8];
+            Vector3 v1 = this.corners[edge & 0x00FF];
+
+            Plane plane = new Plane(v0, v1, viewPos);
+            this.cullPlanes.Add(plane);
+        }
+        this.edges.Clear();
+
+        return this.cullPlanes;
+    }
+
+    public Vector3[] getContour(Vector3 viewPos, Vector3 viewDir)
+    {
+        foreach (Face face in this.faces)
+        {
+            if (Vector3.Dot(face.plane.normal, viewDir) < 0)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    int edge = face.egdes[i];
+                    int edgeCCW = face.egdesCCW[i];
+                    if (this.edges.Contains(edgeCCW))
+                        this.edges.Remove(edgeCCW);
+                    else
+                        this.edges.Add(edge);
+                }
+            }
+        }
+
+        List<int> contourIdx = new List<int>();
+        foreach (int edge in this.edges)
+        {
+            int idx0 = edge >> 8;
+            int idx1 = edge & 0x00FF;
+
+            if (contourIdx.Contains(idx0) == false)
+                contourIdx.Add(idx0);
+
+            if (contourIdx.Contains(idx1) == false)
+                contourIdx.Add(idx1);
+        }
+        this.edges.Clear();
+
+        Vector3[] contour = new Vector3[contourIdx.Count];
+        for(int i = 0; i < contour.Length; i++)
+        {
+            contour[i] = this.corners[contourIdx[i]];
+        }
+
+        return contour;
     }
 
     private void OnDrawGizmos()
@@ -33,4 +237,46 @@ public class Occluder : MonoBehaviour {
         //Gizmos.color = Color.red;
         //Gizmos.DrawWireMesh(AntiPortalHelper.GetCube(), this.transform.position, this.transform.rotation, Vector3.Scale(this.extends, this.transform.localScale) * 2.0f);
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        //if(this.transform.hasChanged)
+        //{
+        //    if (this.corners != null)
+        //    {
+        //        Vector3 min = this.center - extends;
+        //        Vector3 max = this.center + extends;
+        //        this.corners[0].Set(min.x, min.y, min.z);
+        //        this.corners[1].Set(min.x, min.y, max.z);
+        //        this.corners[2].Set(max.x, min.y, max.z);
+        //        this.corners[3].Set(max.x, min.y, min.z);
+        //        this.corners[4].Set(min.x, max.y, min.z);
+        //        this.corners[5].Set(min.x, max.y, max.z);
+        //        this.corners[6].Set(max.x, max.y, max.z);
+        //        this.corners[7].Set(max.x, max.y, min.z);
+
+        //        for (int i = 0; i < this.corners.Length; i++)
+        //        {
+        //            this.corners[i] = this.transform.TransformPoint(this.corners[i]);
+        //        }
+        //    }
+
+        //    this.transform.hasChanged = false;
+        //}
+
+        if (this.corners != null)
+        {
+            Gizmos.color = Color.black;
+            foreach (Vector3 corner in this.corners)
+            {
+                Gizmos.DrawSphere(corner, 0.2f);
+            }
+
+            GUIStyle style = new GUIStyle();
+            style.fontSize = 17;
+            UnityEditor.Handles.Label(this.transform.position, this.screenArea.ToString(), style);
+        }
+    }
+#endif
 }
